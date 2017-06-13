9b265c2319f0899960264fcbcd871640c4b3d6e2
Pete Lazuran and david rolfs <pete+david@>
2017-06-13 11:37:06 -0700

    add join statement in GetStudents method

--- Objects/Course.cs
+++ Objects/Course.cs
@@ -214,49 +214,30 @@ namespace Registrar.Objects
       SqlConnection conn = DB.Connection();
       conn.Open();
 
-      SqlCommand cmd = new SqlCommand("SELECT student_id FROM student_courses WHERE course_id = @CourseId;", conn);
+      SqlCommand cmd = new SqlCommand("SELECT students.* FROM courses JOIN student_courses ON (course_id = student_courses.course_id) JOIN students ON (student_courses.student_id = students.id) WHERE courses.id = @CourseId;", conn);
       SqlParameter courseIdParameter = new SqlParameter();
       courseIdParameter.ParameterName = "@CourseId";
       courseIdParameter.Value = this.GetId();
+
       cmd.Parameters.Add(courseIdParameter);
 
       SqlDataReader rdr = cmd.ExecuteReader();
 
-      List<int> studentIds = new List<int> {};
-      while(rdr.Read())
-      {
-        int studentId = rdr.GetInt32(0);
-        studentIds.Add(studentId);
-      }
-      if (rdr != null)
-      {
-        rdr.Close();
-      }
-
       List<Student> students = new List<Student> {};
-      foreach (int studentId in studentIds)
-      {
-        SqlCommand studentQuery = new SqlCommand("SELECT * FROM students WHERE id = @StudentId;", conn);
 
-        SqlParameter studentIdParameter = new SqlParameter();
-        studentIdParameter.ParameterName = "@StudentId";
-        studentIdParameter.Value = studentId;
-        studentQuery.Parameters.Add(studentIdParameter);
+      while(rdr.Read())
+        {
+          int thisStudentId = rdr.GetInt32(0);
+          string studentName = rdr.GetString(1);
+          string studentDoe = rdr.GetString(2);
+          Student foundStudent = new Student(studentName, studentDoe, thisStudentId);
+          students.Add(foundStudent);
+        }
+        if (rdr != null)
+        {
+          rdr.Close();
+        }
 
-        SqlDataReader queryReader = studentQuery.ExecuteReader();
-        while(queryReader.Read())
-        {
-              int thisStudentId = queryReader.GetInt32(0);
-              string studentName = queryReader.GetString(1);
-              string studentDoe = queryReader.GetString(2);
-              Student foundStudent = new Student(studentName, studentDoe, thisStudentId);
-              students.Add(foundStudent);
-        }
-        if (queryReader != null)
-        {
-          queryReader.Close();
-        }
-      }
       if (conn != null)
       {
         conn.Close();
